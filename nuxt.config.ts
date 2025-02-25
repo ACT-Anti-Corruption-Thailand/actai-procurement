a3a5c36c3363f715b4aa8b3291e1779da10597a3
Poppap-Windows <pimpatipan@punchup.world>
2025-02-25 20:42:13 +0700

    chore: change api link to prod link

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       baseUrl: BASE_URL,
-      apiUrl: ' https://act-api.meeboon.dev',
+      apiUrl: 'https://admin-procurement.actai.co',
     },
   },
   app: {
